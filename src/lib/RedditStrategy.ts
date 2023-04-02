63240970abf3439d71b06af50beb416f1b66265e
April <april@dummy.cafe>
2023-04-02 02:12:58 +0200

    add license + copyright notice(s)

--- src/lib/RedditStrategy.ts
+++ src/lib/RedditStrategy.ts
@@ -1,4 +1,12 @@
-// this strategy comes from https://github.com/oralekin/oth-verification/blob/place/server/lib/RedditStrategyBad.js
+/*
+	Copyright (c) 2023 April <april@dummy.cafe>
+
+	This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+*/
 
 import * as OAuth2 from 'passport-oauth2';
 import * as passport from 'passport';
